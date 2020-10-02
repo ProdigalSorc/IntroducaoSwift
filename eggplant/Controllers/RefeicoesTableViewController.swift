5ed70063c8332af4b162b189c282cd569b3bcf6b
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2020-10-02 13:59:08 -0300

    Configurando a Table View

---
+++ eggplant/Controllers/RefeicoesTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RefeicoesTableViewController.swift
+//  eggplant
+//
+//  Created by Rodrigo Paixão on 02/10/20.
+//  Copyright © 2020 Rodrigo Paixão. All rights reserved.
+//
+
+import UIKit
+
+class RefeicoesTableViewController: UITableViewController {
+
+    let refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
+                     Refeicao(nome: "Pizza", felicidade: 5),
+                     Refeicao(nome: "Comida Japonesa", felicidade: 1),]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao.nome
+        return celula
+    }
+}
