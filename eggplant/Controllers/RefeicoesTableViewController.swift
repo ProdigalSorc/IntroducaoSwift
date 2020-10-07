d019fd9f730e080a888021c4a0b3c8cc7ecc9f47
rodrigopaixaoMac <rodrigo.de.f.paixao@gmail.com>
2020-10-07 14:39:26 -0300

    Alertas com UIAlertController

--- eggplant/Controllers/RefeicoesTableViewController.swift
+++ eggplant/Controllers/RefeicoesTableViewController.swift
@@ -32,7 +32,15 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
     
     @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer) {
         if gesture.state == .began {
-            
+            let celula = gesture.view as! UITableViewCell
+            guard let indexPath = tableView.indexPath(for: celula) else {
+                return
+            }
+            let refeicao = refeicoes[indexPath.row]
+            let alerta = UIAlertController(title: refeicao.nome, message: "felicidade: \(refeicao.felicidade)", preferredStyle: .alert)
+            let botaoCancelar = UIAlertAction(title: "cancelar", style: .cancel, handler: nil)
+            alerta.addAction(botaoCancelar)
+            present(alerta, animated: true, completion: nil)
         }
     }
     
